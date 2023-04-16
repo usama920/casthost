33b95186e90f365a7f13d20e58c0e055778c760c
Usama Aslam <you@example.comawesomeusama92@gmail.com>
2023-04-16 16:22:46 +0500

    27th

--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Admin\StorePaymentController;
 use App\Http\Controllers\Controller;
+use App\Models\SubscriptionPrice;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -20,6 +23,28 @@ class ProfileController extends Controller
             'name' => 'required'
         ]);
         $user = User::find(Auth::user()->id);
+
+        $SubscriptionInfo = SubscriptionPrice::where(['user_id' => Auth::user()->id, 'price' => $request->price])->first();
+
+        if ($SubscriptionInfo) {
+            $user->subscription_price_id = $SubscriptionInfo->id;
+            SubscriptionPrice::where(['user_id' => Auth::user()->id])->update(['status' => 0]);
+            SubscriptionPrice::where(['id' => $SubscriptionInfo->id])->update(['status' => 1]);
+        } else {
+            SubscriptionPrice::where(['user_id' => Auth::user()->id])->update(['status' => 0]);
+
+            $price_id = StorePaymentController::createPrice($request->price);
+            $SubscriptionInfo = new SubscriptionPrice();
+            $SubscriptionInfo->user_id = Auth::user()->id;
+            $SubscriptionInfo->price_id = $price_id;
+            $SubscriptionInfo->product_id = env('PRODUCT_ID');
+            $SubscriptionInfo->price = $request->price;
+            $SubscriptionInfo->status = 1;
+            $SubscriptionInfo->save();
+
+            $user->subscription_price_id = $SubscriptionInfo->id;
+        }
+
         $user->name = $request->name;
         $user->facebook = $request->facebook;
         $user->twitter = $request->twitter;
